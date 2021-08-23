fb6eac771a297b2b7443d2298ae4dc8689dd1294
Donggggg <ptsaturn68@daum.net>
2021-08-24 03:03:37 +0900

    [chore] etc 변경

--- sequelize/models/stock.js
+++ sequelize/models/stock.js
@@ -8,17 +8,18 @@ module.exports = (sequelize, DataTypes) => {
 
   Stock.init(
     {
-      STK_ID: {
-        type: DataTypes.STRING,
+      STK_CD: {
+        type: DataTypes.STRING(10),
         primaryKey: true,
       },
-      STK_CD: DataTypes.STRING(10),
       STK_NM: DataTypes.STRING,
       VL: DataTypes.INTEGER,
       STR_VL: DataTypes.INTEGER,
       END_VL: DataTypes.INTEGER,
       LOW_VL: DataTypes.INTEGER,
       HIGH_VL: DataTypes.INTEGER,
+      AMT: DataTypes.BIGINT,
+      RATE: DataTypes.FLOAT,
     },
     {
       sequelize,
